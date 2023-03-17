f30ef6687972b303751e5cd284af451522ae24a4
Aman Kumar <amanofficial16@gmail.com>
2023-03-17 20:11:30 +0530

    changes made

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
@@ -14,13 +15,29 @@ connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running successfully");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running successfully");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ---------------------- Deployment ------------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  });
+}
+
+// ---------------------- Deployment ------------------------
 
 app.use(notFound);
 app.use(errorHandler);
